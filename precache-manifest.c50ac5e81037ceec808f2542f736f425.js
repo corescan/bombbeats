bc0cfe081b3a83681ecbfdb4b06beb2db107f127
Chris Orescan <csorescan@gmail.com>
2019-12-03 14:55:24 -0600

    Updates

---
+++ precache-manifest.c50ac5e81037ceec808f2542f736f425.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00bc059de3fbb038e474cec730e0c57",
+    "url": "/bombbeats/index.html"
+  },
+  {
+    "revision": "b3ef8f61ad188311309b",
+    "url": "/bombbeats/static/css/main.8f768872.chunk.css"
+  },
+  {
+    "revision": "8d10068446253651dc3d",
+    "url": "/bombbeats/static/js/2.26330197.chunk.js"
+  },
+  {
+    "revision": "b3ef8f61ad188311309b",
+    "url": "/bombbeats/static/js/main.5e246bc6.chunk.js"
+  },
+  {
+    "revision": "0a742fefdd4a65d7ff2e",
+    "url": "/bombbeats/static/js/runtime-main.025281be.js"
+  },
+  {
+    "revision": "faea9d0d80ae22e0d40955191fc822b4",
+    "url": "/bombbeats/static/media/Hat_XL_13.faea9d0d.wav"
+  },
+  {
+    "revision": "e1d4e232f661036cf7be97dac0e5833c",
+    "url": "/bombbeats/static/media/RT_Ian_Chang_Kick_1_Drums_Ian_Chang_one_shot.e1d4e232.wav"
+  },
+  {
+    "revision": "bb5dc9f1102359fbab93d10644474975",
+    "url": "/bombbeats/static/media/RT_Ian_Chang_Tea_Towel_Snare_1_Medium_1_Drums_Ian_Chang_one_shot.bb5dc9f1.wav"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/bombbeats/static/media/digital-7 (mono).58045dab.ttf"
+  }
+]);
